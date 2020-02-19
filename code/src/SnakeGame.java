754f3f8aa5e7ff3c214fdb37fccfe35b17719987
jbguzman <jbguzman@miners.utep.edu>
2020-02-19 14:45:44 -0700

    Create SnakeGame class, class elements, and class methods.

---
+++ code/src/SnakeGame.java
@@ -0,0 +1,47 @@
+public class SnakeGame {
+    private boolean [][] game;
+    private int[] headPosition;
+    private static int exhaustiveChecks;
+    private static int recursiveChecks;
+
+    //comment here
+    public SnakeGame() {
+        boolean[][] board = new boolean[1][1];
+    }
+
+    public SnakeGame(boolean[][] board, int x, int y){
+        this.game = boolean [][] board;
+        int[0] headPosition = x;
+        int[1] headPosition = y;
+
+    }
+
+    public int[] findTailExhaustive(){
+
+    }
+
+    public int[] findTailRecursive(){
+
+    }
+
+    private int[] findTailRecusive(int[] currentPosition, int [] previousPosition){
+
+    }
+
+    private void resetCounters(){
+        exhaustiveChecks = 0;
+        recursiveChecks = 0;
+    }
+
+    private static int getRecursiveChecks(){
+
+    }
+
+    private static int getExhaustiveChecks(){
+
+    }
+
+
+
+
+}
